730dd6ccbe88bda064e1d75981041a7abd9c2808
turim0 <ivan_medvedev_1996@mail.ru>
2019-04-10 21:54:44 +0300

    fixed all issues

--- image_resize.py
+++ image_resize.py
@@ -15,14 +15,13 @@ def create_parser():
 
 
 def validate_params(width, height, scale):
-    if (width or height) and scale is not None:
-        return None
+    return bool(width or height) is not bool(scale)
 
 
-def scale_process(width, height, scale, initial_width, initial_height):
+def process_params(width, height, scale, initial_width, initial_height):
     if scale is not None:
-        width = initial_width * scale
-        height = initial_height * scale
+        width = int(initial_width * scale)
+        height = int(initial_height * scale)
         return width, height
     elif width is None and height is not None:
         ratio = height / initial_height
@@ -37,11 +36,14 @@ def scale_process(width, height, scale, initial_width, initial_height):
 
 
 def name_resized_image(width, height, path_to_original):
-    size_info = '__' + str(width) + 'x' + str(height)
+    size_info = '__{width}x{height}'.format(width=str(width),height=str(height))
     filename = os.path.basename(path_to_original)
-    ext = os.path.splitext(filename)[1]
-    image_name = os.path.splitext(filename)[0]
-    resized_image_name = image_name + size_info + ext
+    image_name, ext = os.path.splitext(filename)
+    resized_image_name = '{name}{size}{ext}'.format(
+        name=image_name,
+        size=size_info, 
+        ext=ext
+    )
     return resized_image_name
 
 
@@ -50,43 +52,41 @@ def resize_image(image, width, height):
     return resized_image
 
 
-def warning(width, height, initial_width, initial_height):
+def display_warning(width, height, initial_width, initial_height):
     if round((width/initial_width), 2) != round((height/initial_height), 2):
         print('Warning: the picture may be disproportionate')
 
 
 def save_image(image, output, resized_image_name):
-    ext = os.path.splitext(resized_image_name)[1]
+    output_path = '{output}/{name}'.format(output=output, name=resized_image_name)
     try:
-        if ext == '.jpg' or ext == '.jpeg':
-            image.save(output+'/'+resized_image_name, 'jpeg')
-        elif ext == '.png':
-            image.save(output + '/' + resized_image_name, 'png')
-    except (ValueError, IOError) as err:
-        print(err)
+            image.save(output_path)
+    except (ValueError, IOError):
+        return False
 
 
 if __name__ == '__main__':
     parser = create_parser()
-    namespace = parser.parse_args()
-    path_to_original = namespace.path_to_original
-    width = namespace.width
-    height = namespace.height
-    scale = namespace.scale
-    output = namespace.output
-    if validate_params(width, height, scale) is None:
-        sys.exit('Variable scale is not compatible with height and weight')
+    args = parser.parse_args()
+    path_to_original = args.path_to_original
+    width = args.width
+    height = args.height
+    scale = args.scale
+    output = args.output
+    if validate_params(width, height, scale) is False:
+        sys.exit('Invalid parameters')
     try:
         image = Image.open(path_to_original)
     except IOError as err:
-        print(err)
-        sys.exit()
+        sys.exit(err)
     initial_width, initial_height = image.size
-    width, height = scale_process(width, height, scale, initial_width, initial_height)
+    width, height = process_params(width, height, scale, initial_width, initial_height)
     resized_image = resize_image(image, width, height)
-    warning(width, height, initial_width, initial_height)
+    display_warning(width, height, initial_width, initial_height)
     resized_image_name = name_resized_image(width, height, path_to_original)
-    save_image(resized_image, output, resized_image_name)
+    saving = save_image(resized_image, output, resized_image_name)
+    if saving is False:
+        print('File saving error')
 
 
 
